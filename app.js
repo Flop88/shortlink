e2b42ec7b1a94f413ac10a5329566d77ae26cae4
MVLikhachev <wwwqwwwqwwwqwwwq@gmail.com>
2021-03-03 23:06:41 +0300

    connecting to mongoDB

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require('express')
+const config = require('config')
+const mongoose = require('mongoose')
 
 const app = express()
 
-app.listen(500, () => {
-    console.log('App has been started')
+const PORT = config.get('port') || 5000
+
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongoUri'), )
+    } catch (e) {
+        console.log('Server Error', e.message)
+        process.exit(1)
+    }
+}
+
+start()
+
+app.listen(PORT, () => {
+    console.log(`App has been started on port ${PORT}...`)
 })
